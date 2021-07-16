3d0ad466b6da05058d8f5e7521ac8185845dd121
JJungwoo <jungwoo5759@gmail.com>
2021-07-16 22:43:09 +0900

    feature: swagger2 설정

--- src/main/java/com/practice/springboot/webservice/board/controller/BoardController.java
+++ src/main/java/com/practice/springboot/webservice/board/controller/BoardController.java
@@ -1,4 +1,14 @@
 package com.practice.springboot.webservice.board.controller;
 
+import io.swagger.annotations.Api;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(value = "/board/v1")
+@Api(tags = {"게시판 API"})
+@RequiredArgsConstructor
 public class BoardController {
+
 }
