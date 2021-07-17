236c2c892163b0622bfd54aa478224ba52d87142
JJungwoo <jungwoo5759@gmail.com>
2021-07-17 22:11:13 +0900

    testcode: 게시판 테스트 코드 추가

---
+++ src/test/java/com/practice/springboot/webservice/board/controller/BoardControllerTest.java
@@ -0,0 +1,30 @@
+package com.practice.springboot.webservice.board.controller;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@SpringBootTest
+class BoardControllerTest {
+
+    @Test
+    public void Post_조회() {
+
+    }
+
+    @Test
+    public void Post_등록() {
+
+    }
+
+    @Test
+    public void Post_수정() {
+
+    }
+
+    @Test
+    public void Post_삭제() {
+
+    }
+}
