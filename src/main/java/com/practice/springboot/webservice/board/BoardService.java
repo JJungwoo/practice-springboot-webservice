10b0196bf9daa51ec738876372e9a0512c35449f
JJungwoo <jungwoo5759@gmail.com>
2021-07-19 23:39:23 +0900

    feature: board crud 서비스 추가

--- src/main/java/com/practice/springboot/webservice/board/BoardService.java
+++ src/main/java/com/practice/springboot/webservice/board/BoardService.java
@@ -2,10 +2,28 @@ package com.practice.springboot.webservice.board;
 
 import org.springframework.stereotype.Service;
 
+import javax.transaction.Transactional;
+
 @Service
 public class BoardService {
 
+    @Transactional
     public BoardRes getBoard() {
         return BoardRes.builder().build();
+    }
+
+    @Transactional
+    public void createBoard() {
+
+    }
+
+    @Transactional
+    public void updateBoard() {
+
+    }
+
+    @Transactional
+    public void deleteBoard() {
+
     }
 }
