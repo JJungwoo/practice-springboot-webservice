7af82b01e74e835be3e0fca58a27fa3e9a5abb45
JJungwoo <jungwoo5759@gmail.com>
2021-07-19 23:32:27 +0900

    feature: board dao 정보 추가

--- src/main/java/com/practice/springboot/webservice/board/BoardDao.java
+++ src/main/java/com/practice/springboot/webservice/board/BoardDao.java
@@ -1,5 +1,26 @@
 package com.practice.springboot.webservice.board;
 
+import lombok.Builder;
+import lombok.Generated;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+@NoArgsConstructor
+@Entity
+@Builder
 public class BoardDao {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(length = 500, nullable = false)
+    private String title;
+
+    private String author;
+
+    @Column(columnDefinition = "TEXT", nullable = false)
+    private String contents;
 
 }
