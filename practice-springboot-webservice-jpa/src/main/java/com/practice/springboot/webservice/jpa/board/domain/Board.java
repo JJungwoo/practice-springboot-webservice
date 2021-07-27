33c1725896b8203750f139106cf240b87ee97453
JJungwoo <jungwoo5759@gmail.com>
2021-07-27 20:55:42 +0900

    feature: jpa board 설정

--- practice-springboot-webservice-jpa/src/main/java/com/practice/springboot/webservice/jpa/board/domain/Board.java
+++ practice-springboot-webservice-jpa/src/main/java/com/practice/springboot/webservice/jpa/board/domain/Board.java
@@ -1,4 +1,21 @@
 package com.practice.springboot.webservice.jpa.board.domain;
 
+import lombok.Getter;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
+@Entity
+@Getter
 public class Board {
+
+    @Id
+    private Long id;
+
+    private String title;
+
+    private String author;
+
+    private String contents;
+
 }
