79d45c15185bffe27c71a94ef2474814d8403805
jungwoo <jungwoo5759@gmail.com>
2021-11-14 14:05:36 +0900

    feat: chapter 02 타코 주문 및 개인 정보 입력 창 추가

---
+++ practice-springboot-webservice-springinaction/src/main/java/com/practice/springinaction/domain/Taco.java
@@ -0,0 +1,20 @@
+package com.practice.springinaction.domain;
+
+import lombok.Data;
+
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+import java.util.List;
+
+@Data
+public class Taco {
+
+    @NotNull
+    @Size(min=5, message="Name must be at least 5 characters long")
+    private String name;
+
+    // 책에는 NotNull 체크가 없는데 ingredients 리스트가 null 일 때 즉, 한개도 체크가 안되었을 때 valid 처리가 안된다.. 추가 필요.
+    @NotNull
+    @Size(min=1, message="You must choose at least 1 ingredient")
+    private List<String> ingredients;
+}
