082c17c188877cc134a2f20cdce459201a08cf7e
JJungwoo <jungwoo5759@gmail.com>
2023-02-18 13:56:32 +0900

    feat: practice redis project init

---
+++ practice-springboot-webservice-redis/src/main/java/com/practice_springboot_webservice_redis/PracticeSpringbootWebserviceRedisApplication.java
@@ -0,0 +1,14 @@
+package com.practice_springboot_webservice_redis;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PracticeSpringbootWebserviceRedisApplication {
+	
+	public static void main(String[] args) {
+		SpringApplication.run(
+				PracticeSpringbootWebserviceRedisApplication.class, args);
+	}
+	
+}
---
+++ practice-springboot-webservice-redis/src/test/java/com/practice_springboot_webservice_redis/PracticeSpringbootWebserviceRedisApplicationTests.java
@@ -0,0 +1,13 @@
+package com.practice_springboot_webservice_redis;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PracticeSpringbootWebserviceRedisApplicationTests {
+	
+	@Test
+	void contextLoads() {
+	}
+	
+}
